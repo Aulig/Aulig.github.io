b80b9464e1d1fe2e6d14a41ec9c630916f946b76
Aulig <janbunk87493@gmail.com>
2019-11-30 16:35:52 +0100

    Add rainbow & first scrolling test

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 
 function onDeviceProximityChanged(event) {
-  document.getElementById('distanceValue').innerHTML = event.value + ' cm (' + event.min + '-' + event.max + ' cm range)';
-  
+	
+	document.getElementById('distanceValue').innerHTML = event.value + ' cm (' + event.min + '-' + event.max + ' cm range)';
 }
 
 function onUserProximityChanged(event) {
-  document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'rather far';
+
+	if (event.near == 'near') {
+		
+		window.scrollBy(0, 100);
+	}
+
+	document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'far away';
 }
 
 function updateLight(event) {
 	
-  document.getElementById("lightValue").innerHTML = event.value + " lux";
+	document.getElementById("lightValue").innerHTML = event.value + " lux";
 }
+
+
 
 window.addEventListener('deviceproximity', onDeviceProximityChanged);
 window.addEventListener('userproximity', onUserProximityChanged);
 
 if ("ondevicelight" in window) {
 	  
-  window.addEventListener("devicelight", updateLight);
+	window.addEventListener("devicelight", updateLight);
 }
