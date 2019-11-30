37d676b351106d2dcb241f7ee1192e653d60fa52
Aulig <janbunk87493@gmail.com>
2019-11-30 15:47:41 +0100

    Clean up demo

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-var box = document.getElementById('box');
 
 function onDeviceProximityChanged(event) {
-  document.getElementById('deviceValue').innerHTML = event.value + ' cm (' + event.min + '-' + event.max + ' cm range)';
+  document.getElementById('distanceValue').innerHTML = event.value + ' cm (' + event.min + '-' + event.max + ' cm range)';
   
-  var size = Math.min(200, Math.max(20, 500 / (event.value || 1)));
-  
-  box.style.width = size + 'px';
-  box.style.height = size + 'px';
 }
 
 function onUserProximityChanged(event) {
   document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'rather far';
-  box.style.backgroundColor = event.near ? 'red' : 'green';
 }
 
 function update(illuminance) {
-  document.getElementById("value").innerHTML = illuminance + " lux";
-
-  var colorPart = Math.min(255, illuminance).toFixed(0);
-  document.getElementById("lightintensitybox").style.backgroundColor =
-    "rgb(" + colorPart + ", " + colorPart + ", " + colorPart + ")";
+  document.getElementById("lightValue").innerHTML = illuminance + " lux";
 }
 
 if ("AmbientLightSensor" in window) {
