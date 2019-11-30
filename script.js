9bb535ef9a189de24ecd714119ad71ac351b55c5
Aulig <janbunk87493@gmail.com>
2019-11-30 15:59:51 +0100

    cleaned and fixed script

--- script.js
+++ script.js
@@ -8,25 +8,15 @@ function onUserProximityChanged(event) {
   document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'rather far';
 }
 
-function update(illuminance) {
-  document.getElementById("lightValue").innerHTML = illuminance + " lux";
+function updateLight(event) {
+	
+  document.getElementById("lightValue").innerHTML = event.value + " lux";
 }
 
-if ("AmbientLightSensor" in window) {
-  try {
-    var sensor = new AmbientLightSensor();
-    sensor.addEventListener("reading", function (event) {
-      update(sensor.illuminance);
-    });
-    sensor.start();
-  } catch (e) {
-    console.error(e);
-  }
-}
+window.addEventListener('deviceproximity', onDeviceProximityChanged);
+window.addEventListener('userproximity', onUserProximityChanged);
+
 if ("ondevicelight" in window) {
-  function onUpdateDeviceLight(event) {
-    update(event.value);
-  }
-  
-  window.addEventListener("devicelight", onUpdateDeviceLight);
+	  
+  window.addEventListener("devicelight", updateLight);
 }
