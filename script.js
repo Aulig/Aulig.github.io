6afbda1e8cbe87df1bd7c700bcad20e94f1db365
Aulig <janbunk87493@gmail.com>
2019-12-01 00:10:36 +0100

    add comments and changed text around

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-
+//update text in html to reflect updated proximity data
 function onDeviceProximityChanged(event) {
 	
-	document.getElementById('distanceValue').innerHTML = event.value + ' cm (' + event.min + '-' + event.max + ' cm range)';
+	document.getElementById('distanceValue').innerHTML = toThreeDecimals(event.value) + 'range: cm (' + toThreeDecimals(event.min) + '-' + toThreeDecimals(event.max) + ' cm)';
 }
 
+//parse number to 3 post comma decimals
+function toThreeDecimals(input) {
+	
+	return Number.parseFloat(input).toPrecision(3);
+}
+
+//scroll if proximity sensor obscured
 function onUserProximityChanged(event) {
 
 	if (event.near) {
@@ -52,72 +59,9 @@ function onUserProximityChanged(event) {
 		}, y);
 	}
 
-	document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'far away';
+	document.getElementById('nearValue').innerHTML = event.near ? 'close' : 'far away';
 }
 
-function updateLight(event) {
-	
-	document.getElementById("lightValue").innerHTML = event.value + " lux";
-}
-
-document.getElementById("testbutton").addEventListener ('click',
-    function() { 
-      		
-		var x = 0;
-		var inc = 1;
-		var max = 2000;
-		var y = 1; //delay in milliseconds
-		var prevY = window.scrollY;
-
-		var id = setInterval(function() {
-			
-			window.scrollBy(0, inc);
-			
-			x = x + inc;
-			
-			if (x > (max / 4) + 1) {
-
-				inc = inc / 1.04;
-			}
-			else {
-				
-				inc = inc * 1.08;
-			}
-			
-			if (x >= max) {
-				
-				clearInterval(id);
-				return;
-			}
-			
-			if (inc < 1) {
-				
-				clearInterval(id);
-			}
-			
-			if (prevY == window.scrollY) {
-				
-				document.getElementById('prebox').scrollIntoView();
-				inc = inc * 1.08; //small speed boost to scroll past short "lag" & color inconsistency when jumping to top
-
-			}
-			else {
-				
-				prevY = window.scrollY;
-			}
-		
-		}, y);
-    }, 
-    true);
-
-window.onscroll = function(ev) {
-	
-};
-
+//add event listeners for proximity changes
 window.addEventListener('deviceproximity', onDeviceProximityChanged);
 window.addEventListener('userproximity', onUserProximityChanged);
-
-if ("ondevicelight" in window) {
-	  
-	window.addEventListener("devicelight", updateLight);
-}
