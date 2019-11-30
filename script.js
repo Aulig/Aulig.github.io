8968bda3849a8f94b5d504708e95709de39186a3
Aulig <janbunk87493@gmail.com>
2019-11-30 15:32:44 +0100

    New testing demo code

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function onUserProximityChanged(event) {
   document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'rather far';
   box.style.backgroundColor = event.near ? 'red' : 'green';
 }
+
+function update(illuminance) {
+  document.getElementById("value").innerHTML = illuminance + " lux";
+
+  var colorPart = Math.min(255, illuminance).toFixed(0);
+  document.getElementById("lightintensitybox").style.backgroundColor =
+    "rgb(" + colorPart + ", " + colorPart + ", " + colorPart + ")";
+}
+
+if ("AmbientLightSensor" in window) {
+  try {
+    var sensor = new AmbientLightSensor();
+    sensor.addEventListener("reading", function (event) {
+      update(sensor.illuminance);
+    });
+    sensor.start();
+  } catch (e) {
+    console.error(e);
+  }
+}
+if ("ondevicelight" in window) {
+  function onUpdateDeviceLight(event) {
+    update(event.value);
+  }
+  
+  window.addEventListener("devicelight", onUpdateDeviceLight);
+}
