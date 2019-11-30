1ab402e77acbd8e4636644b22cd34fe4494c6219
Aulig <janbunk87493@gmail.com>
2019-11-30 18:27:14 +0100

    change scrolling and add test button

--- script.js
+++ script.js
@@ -7,11 +7,37 @@ function onDeviceProximityChanged(event) {
 function onUserProximityChanged(event) {
 
 	if (event.near) {
+
+		var x = 0;
+		var inc = 1;
+		var max = 2000;
+		var y = 1; //delay in milliseconds
+
+		var id = setInterval(function() {
+			
+			window.scrollBy(0, inc);
+			
+			x = x + inc;
+			
+			if (x > (max / 4) + 1) {
+
+				inc = inc / 1.04;
+			}
+			else {
+				
+				inc = inc * 1.07;
+			}
+			
+			if (x >= max) {
+				
+				clearInterval(id);
+			}
+		}, y);
 		
-		window.scrollBy({
-		  top: 900,
-		  behavior: 'smooth'
-		});
+		if($(window).scrollTop() + $(window).height() == $(document).height()) {
+
+			document.getElementById('prebox').scrollIntoView();
+		}
 	}
 
 	document.getElementById('nearValue').innerHTML = event.near ? 'near' : 'far away';
@@ -22,13 +48,48 @@ function updateLight(event) {
 	document.getElementById("lightValue").innerHTML = event.value + " lux";
 }
 
+document.getElementById("testbutton").addEventListener ('click',
+    function() { 
+      		
+		var x = 0;
+		var inc = 1;
+		var max = 2000;
+		var y = 1; //delay in milliseconds
+
+		var id = setInterval(function() {
+			
+			window.scrollBy(0, inc);
+			
+			x = x + inc;
+			
+			if (x > (max / 4) + 1) {
+
+				inc = inc / 1.04;
+			}
+			else {
+				
+				inc = inc * 1.07;
+			}
+			
+			if (x >= max) {
+				
+				clearInterval(id);
+			}
+		}, y);
+		
+		if($(window).scrollTop() + $(window).height() == $(document).height()) {
+
+			document.getElementById('prebox').scrollIntoView();
+		}
+    }, 
+    true);
+
 window.onscroll = function(ev) {
 	
 	if($(window).scrollTop() + $(window).height() == $(document).height()) {
 
 		document.getElementById('prebox').scrollIntoView();
     }
-	
 };
 
 window.addEventListener('deviceproximity', onDeviceProximityChanged);
